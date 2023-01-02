5066a02df1cb2dcc005265e31c02183fa15a667d
Ishwar Jagdale <ishwarrules@gmail.com>
2023-01-02 15:18:00 +0530

    added resume link for mobile devices

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,6 +7,9 @@ export default function Navigation() {
     return (
         <nav id={"navigation"} className={"flex w-full h-[100px] px-8 md:px-14 items-center justify-between"}>
             <a href={"/"} className={"z-[100] text-4xl cursor-pointer accent fab fa-typo3"} />
+            <a href={"/Resume-Ishwar-Jagdale.pdf"} className={"md:hidden w-fit w-full hover-btn cursor-pointer text-[14px] hover-accent font-monospace p-4 px-6 ml-auto mr-2"}>
+                Resume
+            </a>
             <div className={`${sideMenu ? "flex" : "hidden md:flex"} flex-col p-8 md:p-0 w-full h-full md:w-auto md:h-auto fixed md:relative top-0 left-0 backdrop-blur-sm bg-[rgba(0,0,0,0.8)] md:bg-transparent justify-center md:flex-row items-start md:items-center z-[100]`}>
                 <ul className={"flex flex-col md:flex-row slate font-monospace text-2xl md:text-[13px] mr-4 tracking-wide"}>
                     <li className={"py-3 md:px-4 md:py-0"}><a className={"hover-accent"} href={"/about"}><span className={"mr-2 accent"}>01.</span><span>About</span></a></li>
